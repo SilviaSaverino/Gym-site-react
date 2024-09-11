6292b633542725060b14af2e338ac68c321ed1af
Silvia Saverino <silviasave89@gmail.com>
2024-09-11 18:10:10 +0100

    add json file with gym members info

---
+++ member-data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: "John D",
+        review: "Joining this gym has been a life-changing decision! The trainers are amazing, and the facilities are top-notch. I've never felt more motivated and supported in my fitness journey.",
+        membership: "Platinum",
+        avatarImg: "member-1.png",
+    },
+    {
+        id: 2,
+        name: "Cody M.",
+        review: "The atmosphere here is fantastic! I love the group classes, and the equipment is always in excellent condition. I've seen great results since I started.",
+        membership: "Silver",
+        avatarImg: "member-2.png",
+    },
+    {
+        id: 3,
+        name: "Yuval A.",
+        review: "What I love most about this gym is the community. Everyone is so encouraging, and the staff is always there to help. It feels like my second home.",
+        membership: "Bronze",
+        avatarImg: "member-3.png",
+    },
+    {
+        id: 4,
+        name: "Hannah W.",
+        review: "I've tried many gyms, but this one stands out for its personalized attention and clean facilities. Highly recommend it to anyone serious about fitness.",
+        membership: "Gold",
+        avatarImg: "member-4.png",
+    },
+
+]
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -6,13 +6,36 @@ import member_1 from '../../assets/images/member-1.png'
 import member_2 from '../../assets/images/member-2.png'
 import member_3 from '../../assets/images/member-3.png'
 import member_4 from '../../assets/images/member-4.png'
+import memberData from '../../../member-data'
 
 const Testimonials = () => {
+
+    const memberInfo = memberData.map(function(member) {
+        return console.log(member) 
+    })
+    
+
   return (
     <div className='testimonials'>
         <img src={next_icon} alt="" className='next-btn'/>
         <img src={back_icon} alt="" className='back-btn'/>
-        <div className="slider"></div>
+        <div className="slider">
+            <ul>
+                <li>
+                    <div className="slide">
+                        <div className="member-info">
+                            <img src={member_1} alt="" />
+                            <div>
+                                <h3>name</h3>
+                                <h4>Membership <span>whatever</span></h4>
+                            </div>
+                            <p>"Joining this gym has been a life-changing decision! The trainers are amazing, and the facilities are top-notch. 
+                                I've never felt more motivated and supported in my fitness journey."</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
