08f0dcd8e5c94653151b56c4a1a2f27e42266037
Silvia Saverino <silviasave89@gmail.com>
2024-09-12 14:29:35 +0100

    fix avatar img path for live site

--- src/Components/Members/Member.jsx
+++ src/Components/Members/Member.jsx
@@ -5,7 +5,7 @@ const Member = ({ member }) => {
     return (
         <div className="slide">
             <div className="member-info">
-            <img src={`public/assets/images/${member.avatarImg}`} alt={`${member.name}`} />
+            <img src={`./public/assets/images/${member.avatarImg}`} alt={`${member.name}`} />
                 <div>
                     <h3>{member.name}</h3>
                     <h4>Gym Membership <span>{member.membership}</span></h4>
