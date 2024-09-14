2b70a9c740ecba854d4e5627272ac1e091766d24
Silvia Saverino <silviasave89@gmail.com>
2024-09-14 16:14:43 +0100

    add form and form functionality from web3forms.com. Hide access key in .env file

--- src/Components/Contact us/Contact.jsx
+++ src/Components/Contact us/Contact.jsx
@@ -4,8 +4,35 @@ import msg_icon from '../../assets/images/msg-icon.png'
 import location_icon from '../../assets/images/location-icon.png'
 import phone_icon from '../../assets/images/phone-icon.png'
 import mail_icon from '../../assets/images/mail-icon.png'
+import white_arrow from '../../assets/images/white-arrow.png'
 
 const Contact = () => {
+    //below scrip from https://docs.web3forms.com/how-to-guides/js-frameworks/react-js/simple-react-contact-form
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", process.env.REACT_APP_THE_GYM_ACCESS_KEY);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
     return (
         <div className='contact'>
             <div className="contact-col">
@@ -28,7 +55,16 @@ const Contact = () => {
                 </ul>
             </div>
             <div className="contact-col">
-
+                <form onSubmit={onSubmit}>
+                    <label>Name</label>
+                    <input type="text" name="name" placeholder="Enter your name" required />
+                    <label>Phone Number</label>
+                    <input type="tel" name="phone" placeholder="Enter your mobile number" required />
+                    <label>Write your message here</label>
+                    <textarea name="message" rows="6" placeholder="Enter your message" required></textarea>
+                    <button type="submit" className='btn dark-btn'>Submit now <img src={white_arrow} alt="icon of a white arrow pointing right" /></button>
+                </form>
+                <span>{result}</span>
             </div>
         </div>
     )
