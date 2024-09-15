195312cbe7b2cb5e88c7c290974a6e1bb380757e
Silvia Saverino <silviasave89@gmail.com>
2024-09-15 23:02:32 +0100

    add functionality to toggle nav menu on smaller screens

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-scroll'
 import './Navbar.css'
 import logo from '../../assets/images/logo-white.png'
+import menu_icon from '../../assets/images/menu-icon.png'
 
 export const Navbar = () => {
 
@@ -17,11 +18,15 @@ export const Navbar = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
 
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img className="logo" src={logo} alt="gym website logo featuring a dumbbell" />
-            <ul>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'} >
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
@@ -31,6 +36,7 @@ export const Navbar = () => {
                 <Link to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact us</button></Link>
                 </li>
             </ul>
+            <img onClick={toggleMenu} className="burger-menu"src={menu_icon} alt="navigation bar burger icon" />
         </nav>
     )
 }
