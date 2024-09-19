2637689bd407bddbd64c8e2abef3939bf4894583
Silvia Saverino <silviasave89@gmail.com>
2024-09-19 13:56:52 +0100

    add x-icon to exit the video once clicked

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import './Video.css'
-import clip from '../../assets/video/dummy-gym-video.mp4'
+import React from 'react';
+import './Video.css';
+import clip from '../../assets/video/dummy-gym-video.mp4';
+import x_icon from '../../assets/images/x-icon.png';
 
-const Video = ({showVideo, setShowVideo}) => {
+const Video = ({ showVideo, setShowVideo }) => {
+    
   return (
     <div className={`video-player ${showVideo ? '' : 'hide'}`}>
+      <div className="video-container">
         <video src={clip} autoPlay muted controls></video>
+        <img
+          src={x_icon}
+          alt="X icon to exit video clip"
+          onClick={() => setShowVideo(false)}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
